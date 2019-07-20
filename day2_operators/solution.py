b164479a21fb82ba99718645bb771752b52f6d54
louis <@>
2019-07-20 23:15:15 +0100

    Fixed error in rounding.

--- day2_operators/solution.py
+++ day2_operators/solution.py
@@ -6,7 +6,7 @@ def solve(meal_cost, tip_percent, tax_percent):
     tip = meal_cost * tip_percent / 100
     tax = meal_cost * tax_percent / 100
     total_cost = meal_cost + tip + tax
-    print(int(total_cost))
+    print(round(total_cost))
 
 
 if __name__ == '__main__':
